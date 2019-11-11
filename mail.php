3f21536815175fcb489e74775e6aad1613ea5b8c
erika5281 <56335843+erika5281@users.noreply.github.com>
2019-11-11 12:47:35 +0900

    Update mail.php

--- mail.php
+++ mail.php
@@ -33,11 +33,11 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 $site_top = "manzovino.favy.jp/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "manzovino55@gmail.com";
+$to = "ea5281@icloud.com";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-$from = "manzovino55@gmail.com";
+$from = "ea5281@icloud.com";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "Email";
